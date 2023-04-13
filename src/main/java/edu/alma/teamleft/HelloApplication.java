418952cc46abee6aa4c9e45eaaae8616d43f53a2
Jade Harbert <harbertjade2@gmail.com>
2023-04-13 11:01:01 -0400

    IntelliJ and packages hell

--- src/main/java/edu/alma/teamleft/HelloApplication.java
+++ src/main/java/edu/alma/teamleft/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println(HelloApplication.class.getResource("edu.alma.teamleft/EightCAP_Scene_Builder.fxml"));
+        System.out.println(HelloApplication.class.getResource("EightCAP_Scene_Builder.fxml"));
 //        System.out.println(HelloApplication.class.getName());
 //        System.out.println(HelloApplication.class.getResource("hello-view.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EightCAP_Scene_Builder.fxml"));
